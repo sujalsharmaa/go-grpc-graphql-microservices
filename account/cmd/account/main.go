1f519b8022dbfd21017ab2052aa1a90f85ab00e5
sujalsharmaa <sujalsharma151@gmail.com>
2024-12-27 22:44:36 +0530

    changed main.go

--- account/cmd/account/main.go
+++ account/cmd/account/main.go
@@ -9,77 +9,71 @@ import (
 	"github.com/akhilsharma90/go-graphql-microservice/account"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/tinrab/retry"
-	_ "github.com/lib/pq" // PostgreSQL driver
+	_ "github.com/lib/pq"
 )
 
-// Config struct that reads environment variables
 type Config struct {
 	DatabaseURL string `envconfig:"DATABASE_URL"`
 	ENV         string `envconfig:"ENV"`
 }
 
-// initDbAccount initializes the database for the production environment
-func initDbAccount(cfg Config) {
-	if cfg.ENV == "prod" {
-		log.Println("Initializing the database...")
-
-		connStr := fmt.Sprintf("postgres://postgres:postgres@%s:5432/postgres", cfg.DatabaseURL)
-
-		var db *sql.DB
-		var err error
-
-		// Retry connecting to the database
-		retry.ForeverSleep(2*time.Second, func(_ int) error {
-			db, err = sql.Open("postgres", connStr)
-			if err != nil {
-				log.Println("Retrying database connection:", err)
-				return err
-			}
-
-			// Ping the database to ensure the connection is valid
-			if pingErr := db.Ping(); pingErr != nil {
-				log.Println("Retrying database ping:", pingErr)
-				return pingErr
-			}
-			return nil
-		})
-		defer db.Close()
-
-		// Execute SQL commands to create tables
-		_, err = db.Exec(`
-			CREATE TABLE IF NOT EXISTS accounts (
-				id CHAR(27) PRIMARY KEY,
-				name VARCHAR(24) NOT NULL
-			);
-		`)
-		if err != nil {
-			log.Fatalf("Failed to execute database initialization script: %v", err)
-		}
-
-		log.Println("Database initialization complete.")
+func getConnectionString(cfg Config) (string, error) {
+	switch cfg.ENV {
+	case "prod":
+		return fmt.Sprintf("postgres://postgres:postgres@%s:5432/postgres", cfg.DatabaseURL), nil
+	case "dev":
+		return cfg.DatabaseURL, nil
+	default:
+		return "", fmt.Errorf("invalid environment: %s", cfg.ENV)
 	}
 }
 
-func main() {
-	// Load configuration from environment variables
-	var cfg Config
-	err := envconfig.Process("", &cfg)
+func initDb(cfg Config) {
+	connStr, err := getConnectionString(cfg)
 	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		log.Fatalf("Failed to determine connection string: %v", err)
 	}
 
-	// Initialize the database if necessary
-	if cfg.ENV == "prod" {
-	initDbAccount(cfg)
+	log.Printf("Initializing the database for environment: %s...\n", cfg.ENV)
+	var db *sql.DB
+
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		db, err = sql.Open("postgres", connStr)
+		if err != nil {
+			log.Println("Retrying database connection:", err)
+			return err
+		}
+
+		if pingErr := db.Ping(); pingErr != nil {
+			log.Println("Retrying database ping:", pingErr)
+			return pingErr
+		}
+		return nil
+	})
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS accounts (
+			id CHAR(27) PRIMARY KEY,
+			name VARCHAR(24) NOT NULL
+		);
+	`)
+	if err != nil {
+		log.Fatalf("Failed to execute database initialization script: %v", err)
 	}
 
-	// Build the repository connection string
-	repoConnStr := fmt.Sprintf(cfg.DatabaseURL)
+	log.Println("Database initialization complete.")
+}
 
-	// Create repository and retry on failure
+func startService(cfg Config) {
+	connStr, err := getConnectionString(cfg)
+	if err != nil {
+		log.Fatalf("Failed to determine connection string: %v", err)
+	}
+
 	var r account.Repository
-	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
-		r, err = account.NewPostgresRepository(repoConnStr)
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		r, err = account.NewPostgresRepository(connStr)
 		if err != nil {
 			log.Println("Retrying connection to repository:", err)
 		}
@@ -87,8 +81,18 @@ func main() {
 	})
 	defer r.Close()
 
-	// Create and start the gRPC service
 	s := account.NewService(r)
 	log.Println("Listening on port 8080 for gRPC...")
 	log.Fatal(account.ListenGRPC(s, 8080))
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	initDb(cfg)
+	startService(cfg)
 }
--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -20,76 +20,79 @@ type Config struct {
 	ENV         string `envconfig:"ENV"`
 }
 
-// initDbOrder initializes the database for the production environment
-func initDbOrder(cfg Config) {
-	if cfg.ENV == "prod" {
-		log.Println("Initializing the database...")
-
-		connStr := fmt.Sprintf("postgres://postgres:postgres@%s:5432/postgres", cfg.DatabaseURL)
-
-		var db *sql.DB
-		var err error
-
-		// Retry connecting to the database
-		retry.ForeverSleep(2*time.Second, func(_ int) error {
-			db, err = sql.Open("postgres", connStr)
-			if err != nil {
-				log.Println("Retrying database connection:", err)
-				return err
-			}
-
-			// Ping the database to ensure the connection is valid
-			if pingErr := db.Ping(); pingErr != nil {
-				log.Println("Retrying database ping:", pingErr)
-				return pingErr
-			}
-			return nil
-		})
-		defer db.Close()
-
-		// Execute SQL script to create tables
-		_, err = db.Exec(`
-			CREATE TABLE IF NOT EXISTS orders (
-				id CHAR(27) PRIMARY KEY,
-				created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-				account_id CHAR(27) NOT NULL,
-				total_price MONEY NOT NULL
-			);
-			CREATE TABLE IF NOT EXISTS order_products (
-				order_id CHAR(27) REFERENCES orders(id) ON DELETE CASCADE,
-				product_id CHAR(27),
-				quantity INT NOT NULL,
-				PRIMARY KEY (product_id, order_id)
-			);
-		`)
-		if err != nil {
-			log.Fatalf("Failed to execute database initialization script: %v", err)
-		}
-
-		log.Println("Database initialization complete.")
+// getConnectionString returns the connection string based on the environment
+func getConnectionString(cfg Config) (string, error) {
+	switch cfg.ENV {
+	case "prod":
+		return fmt.Sprintf("postgres://postgres:postgres@%s:5432/postgres", cfg.DatabaseURL), nil
+	case "dev":
+		return cfg.DatabaseURL, nil
+	default:
+		return "", fmt.Errorf("invalid environment: %s", cfg.ENV)
 	}
 }
 
-func main() {
-	// Load configuration from environment variables
-	var cfg Config
-	err := envconfig.Process("", &cfg)
+// initDb initializes the database based on the provided configuration
+func initDb(cfg Config) {
+	connStr, err := getConnectionString(cfg)
 	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		log.Fatalf("Failed to determine connection string: %v", err)
 	}
 
-	// Initialize the database if necessary
-	if cfg.ENV == "prod" {
-		initDbOrder(cfg)
+	log.Printf("Initializing the database for environment: %s...\n", cfg.ENV)
+	var db *sql.DB
+
+	// Retry connecting to the database
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		db, err = sql.Open("postgres", connStr)
+		if err != nil {
+			log.Println("Retrying database connection:", err)
+			return err
 		}
 
-	// Build the repository connection string
-	repoConnStr := fmt.Sprintf(cfg.DatabaseURL)
+		// Ping the database to ensure the connection is valid
+		if pingErr := db.Ping(); pingErr != nil {
+			log.Println("Retrying database ping:", pingErr)
+			return pingErr
+		}
+		return nil
+	})
+	defer db.Close()
+
+	// Execute SQL script to create tables
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS orders (
+			id CHAR(27) PRIMARY KEY,
+			created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+			account_id CHAR(27) NOT NULL,
+			total_price MONEY NOT NULL
+		);
+		CREATE TABLE IF NOT EXISTS order_products (
+			order_id CHAR(27) REFERENCES orders(id) ON DELETE CASCADE,
+			product_id CHAR(27),
+			quantity INT NOT NULL,
+			PRIMARY KEY (product_id, order_id)
+		);
+	`)
+	if err != nil {
+		log.Fatalf("Failed to execute database initialization script: %v", err)
+	}
+
+	log.Println("Database initialization complete.")
+}
+
+// startService sets up the repository and starts the gRPC service
+func startService(cfg Config) {
+	connStr, err := getConnectionString(cfg)
+	if err != nil {
+		log.Fatalf("Failed to determine connection string: %v", err)
+	}
+
+	var r order.Repository
 
 	// Retry connecting to the repository
-	var r order.Repository
-	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
-		r, err = order.NewPostgresRepository(repoConnStr)
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		r, err = order.NewPostgresRepository(connStr)
 		if err != nil {
 			log.Println("Retrying connection to repository:", err)
 		}
@@ -101,4 +104,19 @@ func main() {
 	s := order.NewService(r)
 	log.Println("Listening on port 8080 for gRPC...")
 	log.Fatal(order.ListenGRPC(s, cfg.AccountURL, cfg.CatalogURL, 8080))
+}
+
+func main() {
+	// Load configuration from environment variables
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	// Initialize the database
+	initDb(cfg)
+
+	// Start the gRPC service
+	startService(cfg)
 }
